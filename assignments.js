5c01ef10b01a931c512a7853310b2d494b15d4f3
Adrian <adriantutuianu01@gmail.com>
2021-06-26 23:20:54 +0100

    linked assignments.js + log values to the console

--- assignments.js
+++
@@ -1,3 +0,0 @@
-let country = "England";
-let continent = "Europe";
-let population = "56 million";
---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,6 @@
+let country = "England";
+let continent = "Europe";
+let population = "56";
+console.log("country");
+console.log("continent");
+console.log("population");
