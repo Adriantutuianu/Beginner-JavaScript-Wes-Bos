d24a18a4dac7b73a9fb72b06572d66b741091926
Adrian <adriantutuianu01@gmail.com>
2021-06-28 21:39:20 +0100

    operator precedence

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -77,14 +77,14 @@ console.log(job);
 // Is working without let const or var
 lastName = "Schmedtmann";
 console.log(lastName);
-*/
+
 
 // Basic operators
 
 // Minus
 const now = 2037;
 const ageJonas = now - 1991;
-const ageSarah = now - 2019;
+const ageSarah = now - 2018;
 console.log(ageJonas, ageSarah);
 
 console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
@@ -109,3 +109,16 @@ console.log(ageJonas > ageSarah); // > < >= <=
 console.log(ageSarah >= 18);
 
 const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; //x = y = 10
+console.log(x, y);
