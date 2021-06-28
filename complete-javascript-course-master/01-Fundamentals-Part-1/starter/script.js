7c39a993df2d9a3d91ad15c3e8ed3e94514489eb
Adrian <adriantutuianu01@gmail.com>
2021-06-28 22:06:30 +0100

    average of values

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -119,6 +119,10 @@ const ageSarah = now - 2018;
 
 console.log(now - 1991 > now - 2018);
 
-let x, y;
-x = y = 25 - 10 - 5; //x = y = 10
+let x, y; // it will calculate from right to left.
+x = y = 25 - 10 - 5; //x = y = 10, x = 10
 console.log(x, y);
+
+// average of values
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
