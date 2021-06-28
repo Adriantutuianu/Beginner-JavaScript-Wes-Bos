9e52e666b66a4f9ff66fef40eb34e643bb11aa24
Adrian <adriantutuianu01@gmail.com>
2021-06-28 19:05:10 +0100

    operators with string

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -89,3 +89,7 @@ console.log(ageJonas, ageSarah);
 
 console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
 // 2**3 means 2 to the power of 3 = 2*2*2
+
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + lastName);
