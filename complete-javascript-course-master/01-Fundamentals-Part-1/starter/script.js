ec6f87725420be4dfb926396e248efddd7569df1
Adrian <adriantutuianu01@gmail.com>
2021-06-28 20:14:12 +0100

    boolean

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -84,7 +84,7 @@ console.log(lastName);
 // Minus
 const now = 2037;
 const ageJonas = now - 1991;
-const ageSarah = now - 2018;
+const ageSarah = now - 2019;
 console.log(ageJonas, ageSarah);
 
 console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
@@ -105,4 +105,7 @@ x--;
 console.log(x);
 
 // Comparison operators
-console.log(ageJonas > ageSarah);
+console.log(ageJonas > ageSarah); // > < >= <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
