943cd702e49672c641b291e5da5e1bfa1eadf34f
Adrian <adriantutuianu01@gmail.com>
2021-06-29 20:05:50 +0100

    Coding challenge 1- point 3

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -150,8 +150,12 @@ const massMark = 78;
 const heightMark = 1.69;
 const massJohn = 92;
 const heightJohn = 1.95;
-console.log(massMark, heightMark, massJohn, heightJohn);
 
 // 2
 const BMIMark = massMark / heightMark ** 2;
 const BMIJohn = massJohn / heightJohn ** 2;
+console.log(BMIMark, BMIJohn);
+
+//3
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(markHigherBMI);
