5904d7d97169824552b7d4f33662aff92c88fb9e
Adrian <adriantutuianu01@gmail.com>
2021-07-01 21:10:53 +0100

    else if {}

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -331,3 +331,12 @@ if (age == 18) console.log("You just become an adult!(loose)");
 
 const favourite = prompt("What is your favourite number?");
 console.log(favourite);
+
+if (favourite == 23) {
+  // "23" == 23
+  console.log("cool! 23 is an amazing number");
+} else if (favourite === 7) {
+  console.log("7 is also a cool number");
+} else {
+  console.log("Number is not 23 or 7");
+}
