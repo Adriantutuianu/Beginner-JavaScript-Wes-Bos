d1f28f6099c3df7d81a7b95943b450ddf4c5832b
Adrian <adriantutuianu01@gmail.com>
2021-07-03 00:23:04 +0100

    coding challenge 4-  2nd solution

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -477,3 +477,9 @@ Hints:
 const bill = 275;
 const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
 console.log(tip);
+
+console.log(
+  `The bill was ${bill} , the tip was ${tip}, the total value was ${
+    bill + tip
+  }.`
+);
