b76300ed1c3d17250d37a51f347426ef7b288e2f
Adrian <adriantutuianu01@gmail.com>
2021-07-03 14:54:20 +0100

    strict mode example

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,2 +1,11 @@
 "use strict"; //activated strict mode for entire script.
 // it will create visible errors for us in certains situations.
+
+// example
+let hasDriversLicence = false;
+const passTest = true;
+
+if (passTest) hasDriverLicence = true; //ommit the s
+if (hasDriversLicence) console.log("I can drive :D");
+
+// without the error it will work
