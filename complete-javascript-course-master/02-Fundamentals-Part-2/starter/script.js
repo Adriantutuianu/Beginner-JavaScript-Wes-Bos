27e5a62c0e7bf3a6d2298bf7792d968fce965622
Adrian <adriantutuianu01@gmail.com>
2021-07-04 16:36:22 +0100

    Arrow function - multiple

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -70,10 +70,12 @@ const age3 = calcAge3(1991);
 console.log(age3);
 
 // Calculate how many years a person has left until retirement
-const yearsUntilRetirement = (birthYear) => {
+const yearsUntilRetirement = (birthYear, firstName) => {
   const age = 2037 - birthYear;
   const retirement = 65 - age;
-  return retirement;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`;
 };
 
-console.log(yearsUntilRetirement(1991));
+console.log(yearsUntilRetirement(1991, "Jonas"));
+console.log(yearsUntilRetirement(1980, "Bob"));
