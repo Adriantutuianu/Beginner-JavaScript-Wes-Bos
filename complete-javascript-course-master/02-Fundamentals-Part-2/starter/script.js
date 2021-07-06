3dfa359e0efc75b89ecc2096fc511838a07085c5
Adrian <adriantutuianu01@gmail.com>
2021-07-06 13:10:23 +0100

    Functions coding challenge1 point 4

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -195,8 +195,8 @@ Apply this to the team's average scores 😉
 const calcAverage = (a, b, c) => (a + b + c) / 3;
 console.log(calcAverage(3, 4, 5));
 // 2 //Test1
-const scoreDolphins = calcAverage(44, 23, 71);
-const scoreKoalas = calcAverage(65, 54, 49);
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
 console.log(scoreDolphins, scoreKoalas);
 // 3
 const checkWinner = function (avgDolphins, avgKoalas) {
@@ -208,4 +208,12 @@ const checkWinner = function (avgDolphins, avgKoalas) {
     console.log("No team wins...");
   }
 };
+checkWinner(scoreDolphins, scoreKoalas);
+
+checkWinner(576, 111);
+
+//Test 2
+scoreDolphins = calcAverage(85, 54, 41);
+scoreKoalas = calcAverage(23, 34, 27);
+console.log(scoreDolphins, scoreKoalas);
 checkWinner(scoreDolphins, scoreKoalas);
