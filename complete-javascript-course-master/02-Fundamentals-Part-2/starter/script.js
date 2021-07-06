647b34c72807a6c50a8bd637b49384588a00e362
Adrian <adriantutuianu01@gmail.com>
2021-07-06 23:24:06 +0100

    Coding challenge 2- array - point 4

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -283,3 +283,7 @@ console.log(bills);
 // 3
 const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 console.log(bills, tips);
+
+// 4
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(total);
