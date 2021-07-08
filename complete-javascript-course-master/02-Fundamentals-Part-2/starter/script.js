c39b504151216636b4d5bdd3840c66e2f7eeaf02
Adrian <adriantutuianu01@gmail.com>
2021-07-08 13:01:43 +0100

    Coding challenge 3- objects point 2

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -379,15 +379,27 @@ Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.9
 tall.
 */
 
-//1
+//1 + 2
 const mark = {
   fullName: "Mark Miller",
   mass: 78,
   height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
 };
 
 const john = {
   fullName: "John Smith",
   mass: 92,
   height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
 };
+
+mark.calcBMI();
+john.calcBMI();
+console.log(mark.bmi, john.bmi);
