3bbf8047999007fab610c29a4f0b981a64505778
Adrian <adriantutuianu01@gmail.com>
2021-07-10 00:31:57 +0100

    Coding challenge 4- Array and loop - point 3

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -565,3 +565,10 @@ const totals = [];
 const calcTip = function (bill) {
   return bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
 };
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(tip + bills[i]);
+}
+console.log(bills, tips, totals);
