d6625aa523da5ad185688c95ec72dfbd6e61f813
Adrian <adriantutuianu01@gmail.com>
2021-07-09 23:45:25 +0100

    Loop inside a loop

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -506,3 +506,12 @@ const jonas = [
 for (let i = jonas.length - 1; i >= 0; i--) {
   console.log(jonas[i]);
 }
+
+// Loop inside of a loop
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`--------Starting exercise ${exercise} `);
+
+  for (let rep = 1; rep < 6; rep++)
+    [console.log(`Lifting weight repetition ${rep}`)];
+}
