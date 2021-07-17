fef196668eec2fa904f56db411e5831c58cf7b8d
Adrian <adriantutuianu01@gmail.com>
2021-07-17 10:36:54 +0100

    Change a content

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -2,3 +2,8 @@
 // First DOM manipulation
 console.log(document.querySelector('.message').textContent);
 // DOM- document object model - structured representation of HTML Documents .Allows Javascript to access HTML elements and styles to manipulate them
+
+// 72.Selecting and manipulating elements
+
+// Change the content
+document.querySelector('.message').textContent = 'Correct number🥳';
