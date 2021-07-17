84dd2b0bc04b5503dc6ce3815847005e02f76fd8
Adrian <adriantutuianu01@gmail.com>
2021-07-17 10:28:06 +0100

    What is DOM

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,3 +1,4 @@
 'use strict';
 // First DOM manipulation
-console.log(document.querySelector('.message'));
+console.log(document.querySelector('.message').textContent);
+// DOM- document object model - structured representation of HTML Documents .Allows Javascript to access HTML elements and styles to manipulate them
