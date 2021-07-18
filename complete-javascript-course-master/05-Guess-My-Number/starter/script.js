230ebf482e75cae4db973916fa42a5a7d81e502d
Adrian <adriantutuianu01@gmail.com>
2021-07-19 00:11:54 +0100

    If/ Else if added

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -31,12 +31,12 @@ document.querySelector('.guess').value = 23;
 
 // 74. Implementing the Game Logic
 
-const number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = number;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
   if (!guess) {
     document.querySelector('.message').textContent = '🛑 No number!';
-  }
+  } else if (guess === secretNumber) document.querySelector('.message').textContent = 'Correct number🥳';
 });
