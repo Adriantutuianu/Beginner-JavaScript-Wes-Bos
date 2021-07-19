95adb769d5ef1253dbc8048823a3c7577be22588
Adrian <adriantutuianu01@gmail.com>
2021-07-19 09:32:07 +0100

    Score - 1

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -32,9 +32,13 @@ document.querySelector('.guess').value = 23;
 // 74. Implementing the Game Logic
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
 document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = '🛑 No number!';
@@ -42,7 +46,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'Correct number🥳';
   } else if (guess > secretNumber) {
     document.querySelector('.message').textContent = 'Too high 💹';
+    score = score - 1;
+    document.querySelector('.score').textContent = score;
   } else if (guess < secretNumber) {
     document.querySelector('.message').textContent = 'Too low 📉';
+    score = score - 1;
+    document.querySelector('.score').textContent = score;
   }
 });
