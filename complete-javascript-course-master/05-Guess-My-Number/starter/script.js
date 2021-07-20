9381c87a19b2b0351706f059d59511e773684035
Adrian <adriantutuianu01@gmail.com>
2021-07-20 23:15:58 +0100

    Coding challenge 1- point 3

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -91,4 +91,11 @@ document.querySelector('.again').addEventListener('click', function () {
 //2
   score = 20;
 secretNumber = Math.trunc(Math.random() * 20) + 1;
+// 3
+document.querySelector('.message').textContent = 'Start guessing...';
+document.querySelector('.score').textContent = score;
+document.querySelector('.number').textContent = "?";
+document.querySelector('.guess').value = "";
+
+
 }
