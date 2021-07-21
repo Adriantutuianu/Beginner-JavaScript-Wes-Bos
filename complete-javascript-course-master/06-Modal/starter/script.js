b5b0a8cb86fc2a80203119d6b4f5a36d72d706ca
Adrian <adriantutuianu01@gmail.com>
2021-07-21 12:43:46 +0100

    Add a hidden class list

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -10,7 +10,13 @@ for (let i = 0; i < btnsOpenModal.length; i++)
     btnsOpenModal[i].addEventListener('click', function () {
       console.log('Button clicked');
       modal.classList.remove('hidden');
+      overlay.classList.remove('hidden');
     })
   );
 
 //   80 Working with classes
+
+btnCloseModal.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
