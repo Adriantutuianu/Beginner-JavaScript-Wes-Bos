58c6f95790236c706e71c624036ddf48e036d679
Adrian <adriantutuianu01@gmail.com>
2021-07-21 12:34:09 +0100

    Button clicked

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -6,4 +6,10 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-  console.log(btnsOpenModal[i].textContent);
+  console.log(
+    btnsOpenModal[i].addEventListener('click', function () {
+      console.log('Button clicked');
+    })
+  );
+
+//   80 Working with classes
