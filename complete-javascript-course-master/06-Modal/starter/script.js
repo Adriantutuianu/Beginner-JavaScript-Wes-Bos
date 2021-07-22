45a62046c18252b5b235c0cd9825a6bf114adf62
Adrian <adriantutuianu01@gmail.com>
2021-07-22 11:03:10 +0100

    function openModal and closeModal

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -3,22 +3,22 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  console.log(
-    btnsOpenModal[i].addEventListener('click', function () {
-      console.log('Button clicked');
-      modal.classList.remove('hidden');
-      overlay.classList.remove('hidden');
-    })
-  );
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
-//   80 Working with classes
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+for (let i = 0; i < btnsOpenModal.length; i++)
+  console.log(btnsOpenModal[i].addEventListener('click', openModal));
+
+//   80 Working with classes
+
 btnCloseModal.addEventListener('click', closeModal);
 // Click outside the modal to close it
 overlay.addEventListener('click', closeModal);
