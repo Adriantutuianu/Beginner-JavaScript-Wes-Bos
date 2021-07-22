872342bad39a9e6ca60023456f92ce577e875aad
Adrian <adriantutuianu01@gmail.com>
2021-07-22 23:59:03 +0100

    Generating a random dice roll

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -4,8 +4,20 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnNew = document.querySelector('.btn--roll');
+const btnNew = document.querySelector('.btn--hold');
 
 //starting condition
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+// 83. Rolling the dice
+
+btnRoll.addEventListener('click', function () {
+  // 1.Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // 2.Display dice
+  // 3. Check the roller 1 : if true, switch to next player
+});
