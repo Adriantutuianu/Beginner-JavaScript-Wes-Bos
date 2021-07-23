e2507330506618f5d1db60de93f1841dbb14168b
Adrian <adriantutuianu01@gmail.com>
2021-07-23 23:44:22 +0100

    Add dice to current score

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -3,6 +3,9 @@
 
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -13,6 +16,8 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+let currentScore = 0;
+
 // 83. Rolling the dice
 
 btnRoll.addEventListener('click', function () {
@@ -22,5 +27,12 @@ btnRoll.addEventListener('click', function () {
   // 2.Display dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
-  // 3. Check the roller 1 : if true, switch to next player
+  // 3. Check the roller 1
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore = currentScore += dice;
+    current0El.textContent = currentScore; //change later
+  } else {
+    // switch to next player
+  }
 });
