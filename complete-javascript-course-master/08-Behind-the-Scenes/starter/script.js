f0022c872f2a6051ec1431e80101d393add2fbaf
Adrian <adriantutuianu01@gmail.com>
2021-07-27 18:33:41 +0100

    Scope of a variable

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -1,3 +1,4 @@
 'use strict';
 //Scoping: how our prgram`s variables are organized and accessed. Where do variables live? or Where can we access a certain variable, and where not?
 //Lexical scoping: Scoping is controlled by placement of functions and blocks in code
+//Scope of a variable: Region of our code where a certain variable can be accessed.
