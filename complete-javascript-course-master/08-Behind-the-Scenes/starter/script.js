f372643cee41f9b2dfe1be212806449af85f9a83
Adrian <adriantutuianu01@gmail.com>
2021-07-27 18:32:27 +0100

    Lexical scoping

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -1,2 +1,3 @@
 'use strict';
 //Scoping: how our prgram`s variables are organized and accessed. Where do variables live? or Where can we access a certain variable, and where not?
+//Lexical scoping: Scoping is controlled by placement of functions and blocks in code
