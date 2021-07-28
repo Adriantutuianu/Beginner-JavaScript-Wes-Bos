17c80e1a63dc9b837f09dc102ffc89ca03279948
Adrian <adriantutuianu01@gmail.com>
2021-07-28 22:23:57 +0100

    Receive 2 return values from a function

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -54,5 +54,6 @@ console.log(main, secondary);
 
 [main, secondary] = [secondary, main];
 console.log(main, secondary);
-
-console.log(restaurant.order(2, 0));
+//Receive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
