682ce465fe4460e529379b3ffacbbe89e0148439
Adrian <adriantutuianu01@gmail.com>
2021-07-28 22:30:58 +0100

    Default values

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -64,3 +64,7 @@ const nested = [2, 4, [5, 6]]; //nested- array inside an array
 
 const [i, , [j, k]] = nested;
 console.log(i, j, k);
+
+//Default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
