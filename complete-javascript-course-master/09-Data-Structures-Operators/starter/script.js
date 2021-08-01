1e54c10bf47b8eead66939bf7ec919fb584e151b
Adrian <adriantutuianu01@gmail.com>
2021-08-01 23:30:09 +0100

    Looping objects final

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -352,4 +352,8 @@ console.log(values);
 //Property entries : Names + values together
 //Entire object
 const entries = Object.entries(openingHours);
-console.log(entries);
+// console.log(entries);
+
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
