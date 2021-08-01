b40fbf40ed189986b6c6b9b08ae9357891c03945
Adrian <adriantutuianu01@gmail.com>
2021-08-01 21:15:15 +0100

    The for-of loop

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -324,8 +324,8 @@ team1 > team2 && console.log('Team 2 is more likely to win');
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 for (const item of menu) console.log(item);
 
-for (const item of menu.entries()) {
-  console.log(item);
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`);
 }
 
 // console.log([...menu.entries()]);
