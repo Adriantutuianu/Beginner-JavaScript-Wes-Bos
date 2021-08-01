d0d2fbf4213bc0fb79caeba1f26ae8374d20aca3
Adrian <adriantutuianu01@gmail.com>
2021-08-01 15:33:15 +0100

    Short circiuting example

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -191,3 +191,9 @@ console.log(true || 0);
 console.log(undefined || null);
 
 console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+// restaurant.numGuests = 23;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
