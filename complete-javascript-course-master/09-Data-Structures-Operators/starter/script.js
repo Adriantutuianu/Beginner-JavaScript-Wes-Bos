cc9042278d295dff8e5a9822de43ae846380c42f
Adrian <adriantutuianu01@gmail.com>
2021-08-02 12:42:32 +0100

    Has method

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -474,3 +474,7 @@ console.log(rest.get(1));
 
 const time = 21; // check if it is open or closed
 console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+console.log(rest);
