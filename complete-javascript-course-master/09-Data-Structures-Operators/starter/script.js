0e1c535a545e02663fbd101e1ecc43c376286fd0
Adrian <adriantutuianu01@gmail.com>
2021-08-03 23:41:37 +0100

    The slice method example- extract parts from strings

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -604,3 +604,14 @@ console.log(airline.slice(0, airline.indexOf(' ')));
 console.log(airline.slice(airline.lastIndexOf(' ')));
 
 console.log(airline.slice(-2)); //Negative value - it starts from the end
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  //B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') console.log('You got the middle seat :)');
+  else console.log('You got lucky :)');
+};
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
