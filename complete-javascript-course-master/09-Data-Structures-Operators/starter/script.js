70b1a8c1f57b7bbed6383c74f38311b32874f502
Adrian <adriantutuianu01@gmail.com>
2021-08-03 23:04:55 +0100

    Coding challenge 3- task 1

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -450,7 +450,7 @@ console.log(
 
 console.log(new Set('adriantutuianu').size); // how many different letters are in a string
 */
-
+/*
 // 116. Maps: Fundamentals
 // map is a data structure that we can use to map values to keys
 const rest = new Map();
@@ -522,3 +522,42 @@ console.log(question.get(question.get('correct') === answer));
 
 //Convert map to an array
 console.log([...question]);
+*/
+// 119 coding challenge 3
+/*
+Let's continue with our football betting app! This time, we have a map called
+'gameEvents' (see below) with a log of the events that happened during the
+game. The values are the events themselves, and the keys are the minutes in which
+each event happened (a football game has 90 minutes plus some extra time).
+Your tasks:
+1. Create an array 'events' of the different game events that happened (no
+duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64
+was unfair. So remove this event from the game events log.
+3. Compute and log the following string to the console: "An event happened, on
+average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over 'gameEvents' and log each element to the console, marking
+whether it's in the first half or second half (after 45 min) of the game, like this:
+[FIRST HALF] 17:
+⚽
+GOAL
+*/
+
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+// 1
+
+const events = [...new Set(gameEvents.values())];
+console.log(events);
