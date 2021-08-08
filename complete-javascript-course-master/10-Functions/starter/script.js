df993e1aecfd8539fda6544ebb4eabea0e854bd6
Adrian <adriantutuianu01@gmail.com>
2021-08-08 20:58:08 +0100

    Higher order functions

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -61,3 +61,8 @@ checkIn(flight, jonas);
 // -JS treats functions as first class citizens
 // - functions are simply values
 // -functions are just another type of object
+
+//Higher order function
+//A function that receives another function as an argument , that returns
+// a new function , or both.
+// This is only posible because of first class functions.
