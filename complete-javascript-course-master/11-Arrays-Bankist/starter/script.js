8c50267ad4b1ddab11d0c6c6848264f9266a36fe
Adrian <adriantutuianu01@gmail.com>
2021-08-14 15:38:31 +0100

    Callback function

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -228,12 +228,14 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 const eurToUsd = 1.1;
 
-const movementsUsd = movements.map(function (mov) {
-  return mov * eurToUsd;
-});
+// const movementsUsd = movements.map(function (mov) {
+//   return mov * eurToUsd;
+// });
+// callback function
+const movementsUsd = movements.map(mov => mov * eurToUsd);
 
 console.log(movements, movementsUsd);
-//Similar
+//Similar result
 const movementsUsdfor = [];
 for (const mov of movements) movementsUsdfor.push(mov * eurToUsd);
 console.log(movementsUsdfor);
