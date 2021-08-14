7cce2a0b0016cfd4cb6eb4dda1f12f94d2777131
Adrian <adriantutuianu01@gmail.com>
2021-08-14 20:06:11 +0100

    The filter method example

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -277,3 +277,8 @@ console.log(deposits);
 const depositsFor = [];
 for (const mov of movements) if (mov > 0) depositsFor.push(mov);
 console.log(depositsFor);
+
+const withdrawals = movements.filter(function (mov) {
+  return mov < 0;
+});
+console.log(withdrawals);
