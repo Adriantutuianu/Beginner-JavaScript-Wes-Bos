1eadd864028eb54c0d7a525fe48e2e49f2fdafdc
Adrian <adriantutuianu01@gmail.com>
2021-08-24 22:17:31 +0100

    Sticky Navigation

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -136,4 +136,7 @@ const initialCoords = section1.getBoundingClientRect();
 console.log(initialCoords);
 window.addEventListener('scroll', function () {
   console.log(window.scrollY);
+
+  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
 });
