b8e130e333bff5bde0a7fd47c63d4275eab403f8
Adrian <adriantutuianu01@gmail.com>
2021-08-24 21:41:15 +0100

    Dom traversing - going upwards: parents

--- complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -108,3 +108,11 @@ const h1 = document.querySelector('h1');
 //Going downwards: child
 
 console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+
+// Going upwards: parents
+
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
