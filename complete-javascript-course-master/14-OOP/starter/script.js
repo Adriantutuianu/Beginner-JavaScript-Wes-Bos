fd70061f282e42ecf931e846dd2ec09345541349
Adrian <adriantutuianu01@gmail.com>
2021-08-25 20:28:08 +0100

    The New Operator

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -1,1 +1,17 @@
 'use strict';
+// Object oriented programming OOP
+
+// OOP i a programming paradigm based on the concept of objects.
+
+// Constructor function and the new Operator
+
+const Person = function (firstName, birthYear) {
+  console.log(this);
+};
+
+new Person('Jonas', 1991);
+
+// 1. New empty object {} is created
+// 2. Function is called, this = {}
+// 3. {} linked to a prototype
+// 4. function automatically return {}
