a4ca5f9fde84af13cb6beb3a9f5afa401b83b605
Adrian <adriantutuianu01@gmail.com>
2021-08-25 20:33:14 +0100

    The New operator example

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -6,12 +6,19 @@
 // Constructor function and the new Operator
 
 const Person = function (firstName, birthYear) {
-  console.log(this);
+  this.firstName = firstName;
+  this.birthYear = birthYear;
 };
 
-new Person('Jonas', 1991);
-
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
 // 1. New empty object {} is created
 // 2. Function is called, this = {}
 // 3. {} linked to a prototype
 // 4. function automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 2000);
+
+console.log(matilda);
+console.log(jack);
