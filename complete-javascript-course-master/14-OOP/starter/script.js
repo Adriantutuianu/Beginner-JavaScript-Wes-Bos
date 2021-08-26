aa00bd2e9a82d4544e1a560f683b30a2b9cbf5bd
Adrian <adriantutuianu01@gmail.com>
2021-08-26 22:03:29 +0100

    Class example

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -172,3 +172,22 @@ const martha = new StudentCL('Martha Jones', 2012, 'computer science');
 console.log(martha);
 
 martha.introduce();
+
+// Class example
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+    console.log(`Thanks for opening an account ${owner}`);
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+console.log(acc1);
+
+acc1.movements.push(250);
+acc1.movements.push(150);
