58bd6c0cb5740287fa5d784e21e228df27eb4e38
Adrian <adriantutuianu01@gmail.com>
2021-08-27 23:12:32 +0100

    Leaflet library L.

--- complete-javascript-course-master/15-Mapty/starter/script.js
+++ complete-javascript-course-master/15-Mapty/starter/script.js
@@ -19,6 +19,18 @@ if (navigator.geolocation)
       const { longitude } = position.coords;
       console.log(latitude, longitude);
       console.log(`http://www.google.pt/maps/@${latitude} ,${longitude}`);
+
+      const map = L.map('map').setView([51.505, -0.09], 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker([51.5, -0.09])
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Could not get your position');
