c080960cc13588648eee52c090e567f1f6fee68c
Adrian <adriantutuianu01@gmail.com>
2021-08-29 15:52:58 +0100

    Ajax Call

--- complete-javascript-course-master/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master/16-Asynchronous/starter/script.js
@@ -4,3 +4,19 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+// Asynchronous js : promises , async / await and ajax
+
+// Ajax Call: XMLHttpRequest
+
+const request = new XMLHttpRequest();
+request.open(
+  'GET',
+  `https://restcountries.eu/rest/v2/name/portugal
+`
+);
+request.send();
+
+request.addEventListener('load', function () {
+  console.log(this.responseText);
+});
