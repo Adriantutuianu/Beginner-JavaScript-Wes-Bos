67e8aa7674ca9350b087fa0cf73d00ef969d7c3d
Adrian <adriantutuianu01@gmail.com>
2021-09-02 23:32:16 +0100

    Default export

--- complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
+++ complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -4,8 +4,9 @@
 // addToCart('bread', 5);
 console.log('Importing module');
 
-import * as ShoppingCart from './shoppingCart.js';
-ShoppingCart.addToCart('bread', 5);
-console.log(ShoppingCart.totalPrice);
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
 
 import add from './shoppingCart.js'; //import default export- any name
+add('pizza', 2);
